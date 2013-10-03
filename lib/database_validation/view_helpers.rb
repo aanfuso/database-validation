d525638a07ab3ed7d91de3e9837fab302d21772a
Agustin Anfuso <anfusoagustin89@gmail.com>
2013-10-03 11:30:52 -0300

    Added form option to skip validations.

--- lib/database_validation/view_helpers.rb
+++ lib/database_validation/view_helpers.rb
@@ -2,7 +2,7 @@ module DatabaseValidation
   module ViewHelpers
     class ActionView::Helpers::FormBuilder
       def text_field_with_maxlength(method, options = {})
-        options[:maxlength] ||= @object.class.limit_for(method)
+        options[:maxlength] ||= @object.class.limit_for(method) unless @options[:skip_validations]
         text_field_without_maxlength(method, options.merge(maxlength: options[:maxlength]))
       end
       alias_method_chain :text_field, :maxlength
